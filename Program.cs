34cbe3e9d4f74faef4b83631bf8bbe9ed792b1f8
Hugzxhugz <jameskenthagman@gmail.com>
2023-02-11 03:51:52 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Channels;
+
+namespace TextToBinaryConverter
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Text - Binary Converter");
+            Console.Write("Please enter text to convert: ");
+            string input = Console.ReadLine();
+            
+            string output = TextToBinary(input);
+            Console.WriteLine("Binary conversion for text is:");
+            Console.WriteLine(output);
+            Console.ReadLine();
+
+        }
+
+        static string TextToBinary(string input)
+        {
+            string output = "";
+            
+            foreach (char c in input.ToArray())
+            {
+                output += Convert.ToString(c, 2).PadLeft(8, '0') + " ";
+            }
+            return output;
+        }
+    }
+}
+
